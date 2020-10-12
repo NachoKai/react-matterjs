f735b126245010d6f63c09897a7f5c60271128e3
Nacho Caiafa <nachokai@outlook.com>
2020-10-12 00:40:44 -0300

    delete unnecessary stuff + install matterjs and add basic app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+import Scene from "./Scene";
+
+const Container = styled.div``;
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Scene />
+    </Container>
   );
-}
+};
 
 export default App;
---
+++ src/Scene.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+import Matter from "matter-js";
+
+const Container = styled.div``;
+
+const Scene = () => {
+  const sceneRef = useRef(null);
+
+  useEffect(() => {
+    const Engine = Matter.Engine,
+      Render = Matter.Render,
+      World = Matter.World,
+      Bodies = Matter.Bodies,
+      Mouse = Matter.Mouse,
+      MouseConstraint = Matter.MouseConstraint;
+
+    const engine = Engine.create({
+      // positionIterations: 20
+    });
+
+    const render = Render.create({
+      element: sceneRef.current,
+      engine: engine,
+      options: {
+        width: 1600,
+        height: 800,
+        wireframes: false,
+      },
+    });
+
+    // balls
+    const ballA = Bodies.circle(0, 0, 30, { restitution: 0.5 });
+    const ballB = Bodies.circle(800, 0, 30, { restitution: 0.5 });
+
+    World.add(engine.world, [
+      // top wall
+      Bodies.rectangle(0, 0, 4000, 50, { isStatic: true }),
+      // bottom wall
+      Bodies.rectangle(0, 800, 4000, 50, { isStatic: true }),
+      // left wall
+      Bodies.rectangle(0, 800, 50, 2000, { isStatic: true }),
+      // center wall
+      Bodies.rectangle(800, 0, 50, 2000, { isStatic: true }),
+      // right wall
+      Bodies.rectangle(1600, 0, 50, 2000, { isStatic: true }),
+    ]);
+
+    World.add(engine.world, [ballA, ballB]);
+
+    // add mouse control
+    const mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false,
+          },
+        },
+      });
+
+    World.add(engine.world, mouseConstraint);
+
+    Matter.Events.on(mouseConstraint, "mousedown", function (event) {
+      World.add(engine.world, Bodies.circle(100, 50, 30, { restitution: 0.7 }));
+      World.add(engine.world, Bodies.circle(900, 50, 30, { restitution: 0.7 }));
+    });
+
+    Engine.run(engine);
+    Render.run(render);
+  }, []);
+
+  return (
+    <Container>
+      <div ref={sceneRef} />
+    </Container>
+  );
+};
+
+export default Scene;
