db16c2008df57e7cb04a32d9534eb7d60bec3c2b
Nacho Caiafa <nachokai@outlook.com>
2020-10-12 01:06:11 -0300

    Updates

---
+++ precache-manifest.61db77946b758c6a48aa72330f153a13.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c7e7dc9109b3d4ca663db2fa098dde",
+    "url": "/react-matterjs/index.html"
+  },
+  {
+    "revision": "f941bd583727bcfd963d",
+    "url": "/react-matterjs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "91821386e00f780c6717",
+    "url": "/react-matterjs/static/js/2.1d361ea5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-matterjs/static/js/2.1d361ea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f941bd583727bcfd963d",
+    "url": "/react-matterjs/static/js/main.7474d42e.chunk.js"
+  },
+  {
+    "revision": "9da8108598a494389caf",
+    "url": "/react-matterjs/static/js/runtime-main.b28fc43c.js"
+  }
+]);
